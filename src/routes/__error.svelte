6b18933f417deec6c6997931f20b7afb1e004b50
Damian Soto <soto.platero@gmail.com>
2022-01-03 22:18:30 -0500

    add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').ErrorLoad} */
+	export function load({ error, status }) {
+		return {
+			props: {
+				status, message: error.message 
+			}
+		};
+	}
+</script>
+
+<script>
+	export let status;
+	export let message;
+</script>
+
+<div class="h-screen flex items-center justify-center px-2">
+	<div class="text-center">
+		<h1 class="text-8xl">{status}</h1>
+		<p class="text-3xl">{message}</p>
+		<p><a href="/">Home</a></p>
+	</div>
+</div>
