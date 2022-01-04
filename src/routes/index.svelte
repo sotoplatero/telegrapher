0bd4a00d0fd1a0e6bd36c858719ab0536d8ae689
Damian Soto <soto.platero@gmail.com>
2022-01-04 08:45:50 -0500

    update front

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,32 +4,37 @@
 	const selectAll = (e) => e.target.select()
 </script>
 
-<header class="text-center mb-10 mt-24">
-	<h1 class="text-6xl font-semibold mb-4">Telegrapher</h1>
-	<p class="text-4xl text-gray-700 px-10 leading-snug">Publish your blog posts to <a href="https://telegra.ph">Telegraph</a> for free</p>
-</header>
-
-<div class="flex items-center space-x-3">
-	<input 
-		type="text" 
-		name="url" 
-		bind:value={url} 
-		on:focus={selectAll}
-		class="w-full p-3 border-2 border-gray-400 rounded-lg disabled:bg-gray-100 focus:select-all" 
-		placeholder="Post URL"
-	>
-
-	<a 
-		href={to} 
-		disabled={!to}  
-		type="submit" 
-		target="_blank" 
-		rel="nofollower noopener"
-		class="px-9 p-3 border-2 border-gray-700 rounded-lg font-semibold disabled:bg-gray-200 flex items-center"
-	>
-		Publish
-	</a>
+<div class="flex min-h-screen items-center justify-center">
+	<div>
 		
+		<header class="text-center mb-10 px-4 sm:px-10">
+			<h1 class="text-xl sm:text-3xl uppercase font-light mb-4 text-gray-700">Telegrapher</h1>
+			<p class="text-2xl sm:text-5xl font-bold text-gray-800 leading-snug">Automatically publish your blog posts for <a class="underline" href="https://telegra.ph" target="_blank">Telegra.ph</a></p>
+		</header>
+
+		<div class="flex flex-col sm:flex-row items-center sm:space-x-3 space-x-0 space-y-3 sm:space-y-0 px-2 ">
+			<input 
+				type="text" 
+				name="url" 
+				bind:value={url} 
+				on:focus={selectAll}
+				class="w-full p-3 border-2 border-gray-400 rounded-lg disabled:bg-gray-100 focus:select-all" 
+				placeholder="Post URL"
+			>
+
+			<a 
+				href={to} 
+				disabled={!to}  
+				type="submit" 
+				target="_blank" 
+				rel="nofollower noopener"
+				class="px-9 p-3 border-2 border-gray-700 rounded-lg font-semibold disabled:bg-gray-200 flex items-center"
+			>
+				Publish
+			</a>
+				
+		</div>
+	</div>
 </div>
 
 
