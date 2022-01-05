87fbdda5b7fc54a8b3203b0ae7568e90499f1727
Damian Soto <soto.platero@gmail.com>
2022-01-05 10:05:55 -0500

    publishing

--- src/routes/to/[...url].svelte
+++ src/routes/to/[...url].svelte
@@ -1,28 +1,9 @@
-<!-- <script context="module">
-	/** @type {import('@sveltejs/kit').Load} */
-	export async function load({ params, fetch}) {
-		const res = await fetch(`/publish/${params.url}`);
-
-		if (res.ok) {
-			const article = await res.json()
-			return {
-				status: 301,
-				redirect: article.url
-			}
-		}
-
-		return {
-			status: res.status,
-			error: new Error(`Could not load ${params.url}`)			
-		}
-
-	}
-</script> -->
-
 <script>
 	import { onMount} from 'svelte'
 	import { goto } from '$app/navigation';	
 	import { page } from '$app/stores';
+
+	let error
 
 	onMount( async () => {
 		const res = await fetch(`/publish/${$page.params.url}`);
@@ -30,10 +11,24 @@
 		if (res.ok) {
 			const article = await res.json()
 			goto(article.url)
+		} else {
+			error = res.status
 		}
+
 	})
+
 </script>
 
 <div class="flex h-screen items-center justify-center">
-	<div class="text-4xl font-bold animate-pulse text-gray-700">Publishing...</div>
+{#if !error}
+	<div class="text-3xl font-bold animate-pulse text-gray-700">Publishing...</div>
+{:else}
+	<div class="text-4xl font-bold text-gray-700">
+		{#if error == 404}
+			Could not load {$page.params.url}
+		{:else}
+			An error occurred, please try later.
+		{/if}
+	</div>
+{/if}
 </div>
