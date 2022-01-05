1375feb24818d4a5312ed0cfdc9e732e5c276892
Damian Soto <soto.platero@gmail.com>
2022-01-05 09:58:00 -0500

    publishing

--- src/routes/to/[...url].svelte
+++ src/routes/to/[...url].svelte
@@ -1,4 +1,4 @@
-<script context="module">
+<!-- <script context="module">
 	/** @type {import('@sveltejs/kit').Load} */
 	export async function load({ params, fetch}) {
 		const res = await fetch(`/publish/${params.url}`);
@@ -17,4 +17,23 @@
 		}
 
 	}
+</script> -->
+
+<script>
+	import { onMount} from 'svelte'
+	import { goto } from '$app/navigation';	
+	import { page } from '$app/stores';
+
+	onMount( async () => {
+		const res = await fetch(`/publish/${$page.params.url}`);
+
+		if (res.ok) {
+			const article = await res.json()
+			goto(article.url)
+		}
+	})
 </script>
+
+<div class="flex h-screen items-center justify-center">
+	<div class="text-4xl font-bold animate-pulse text-gray-700">Publishing...</div>
+</div>
